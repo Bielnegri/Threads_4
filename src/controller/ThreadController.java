48a0fcdef193e3a52084b9349d4884413ee63368
Gabrielnegri <gabriel.benedito.negri@outlook.com>
2025-03-11 15:04:33 -0300

    Primeiro Commit

--- src/controller/ThreadController.java
+++ src/controller/ThreadController.java
@@ -3,22 +3,41 @@ package controller;
 public class ThreadController extends Thread{
 	
 	private int[] vet;
-	private int linha;
+	private int n;
 	
-	public ThreadController(int[] vet, int linha) {
+	public ThreadController(int[] vet, int n) {
 		super();
 		this.vet = vet;
-		this.linha = linha;
+		this.n = n;
 	}
 	
 	@Override
-	public void run() {
-		int soma = 0;
-		
-		for(int i = 0; i < this.vet.length; i++) {
-			soma = soma + vet[i];
+	public void run() {		
+		if(this.n % 2 == 0) {
+			double inicio = System.nanoTime();
+			
+			for(int i = 0; i < this.vet.length; i++) {
+				
+			}
+			
+			double fim = System.nanoTime();
+			double total = fim - inicio;
+			total = total / Math.pow(10, 9);
+			System.out.println("Tempo For: " + total);
+		}
+		else {
+			double inicio = System.nanoTime();
+			
+			for(int n : this.vet) {
+				
+			}
+			
+			double fim = System.nanoTime();
+			double total = fim - inicio;
+			total = total / Math.pow(10, 9);
+			System.out.println("Tempo Foreach: " + total);
+			
 		}
 		
-		System.out.println("Linha " + this.linha + "\nSoma: " + soma);
 	}
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -7,17 +7,15 @@ import controller.ThreadController;
 public class Principal {
 
 	public static void main(String[] args) {
-		int[][] mat = new int[3][5];
+		int[] vet = new int[1000];
 		Random r = new Random();
 		
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 5; j++) {
-				mat[i][j] = r.nextInt(0, 11);
-			}
+		for(int i = 0; i < 1000; i++) {
+			vet[i] = r.nextInt(1, 101);
 		}
 		
-		for(int i = 0; i < 3; i++) {
-			ThreadController tc = new ThreadController(mat[i], i);
+		for(int i = 1; i < 3; i++) {
+			ThreadController tc = new ThreadController(vet, i);
 			tc.start();
 		}
 	}
